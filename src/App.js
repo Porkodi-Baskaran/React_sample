eba9181ce36c6fbbaeaf4f1156b44d5724c0bbf3
porkodi <porkodibaskaran27@gmail.com>
2025-01-14 20:25:11 +0530

    added list and key. learnt useState hook

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import Footer from './Footer';
+import Content from './Content';
+import Header from './Header';
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <Header title="Porkodi Baskaran List"/>
+      <Content />
+      <Footer />
+    </div>
+  )
+} 
+
+
+
+  /*
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+          HI I'm Aishwarya hello
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+  */
+
+
+export default App;
